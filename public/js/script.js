d709b1c4f075a839078b1e6243312fbf687fa5bc
Tam <Tam@TAM-DESKTOP>
2024-01-06 15:42:36 +0700

    Hiển thị thông báo khi thêm vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -18,40 +18,62 @@ var imagesMain = new Swiper(".imagesMain", {
 // End Slider Tour Detail
 
 
+// Thông báo thêm thành công vào giỏ hàng
+const alertAddCartSuccess = () => {
+    const elementAlert = document.querySelector("[alert-add-cart-success]");
+    if(elementAlert) {
+      elementAlert.classList.remove("alert-hidden");
+  
+      setTimeout(() => {
+        elementAlert.classList.add("alert-hidden");
+      }, 3000);
+  
+      const closeAlert = elementAlert.querySelector("[close-alert]");
+      closeAlert.addEventListener("click", () => {
+        elementAlert.classList.add("alert-hidden");
+      });
+    }
+  }
+// hết hàm thông báo
+
+
 // Carts
 
 // Nếu chưa có giỏ hàng trong localStorage thì tạo giỏ hàng mới cho người dùng
 const cart = localStorage.getItem("cart");
-if(!cart) {
-  localStorage.setItem("cart", JSON.stringify([]));
+if (!cart) {
+    localStorage.setItem("cart", JSON.stringify([]));
 }
 
 // Thêm tour vào cart
 const formAddToCart = document.querySelector("[form-add-to-cart]");
-if(formAddToCart) {
-  formAddToCart.addEventListener("submit", (event) => {
-    event.preventDefault();
+if (formAddToCart) {
+    formAddToCart.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    const quantity = parseInt(event.target.elements.quantity.value);
-    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+        const quantity = parseInt(event.target.elements.quantity.value);
+        const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
 
-    if(quantity > 0 && tourId) {
-      const cart = JSON.parse(localStorage.getItem("cart"));
+        if (quantity > 0 && tourId) {
+            const cart = JSON.parse(localStorage.getItem("cart"));
+    
+            const indexExistTour = cart.findIndex(item => item.tourId == tourId);
 
-      const indexExistTour = cart.findIndex(item => item.tourId == tourId);
+            if (indexExistTour == -1) {
+                cart.push({
+                    tourId: tourId,
+                    quantity: quantity
+                });
+            } else {
+                cart[indexExistTour].quantity = cart[indexExistTour].quantity + quantity;
+            }
 
-      if(indexExistTour == -1) {
-        cart.push({
-          tourId: tourId,
-          quantity: quantity 
-        });
-      } else {
-        cart[indexExistTour].quantity = cart[indexExistTour].quantity + quantity;
-      }
+            localStorage.setItem("cart", JSON.stringify(cart));
 
-      localStorage.setItem("cart", JSON.stringify(cart));
-    }
-  });
+            alertAddCartSuccess()
+        }
+    });
 }
 
 // End Carts
+
